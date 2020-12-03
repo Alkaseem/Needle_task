24a1e416f8e2bb3001303e65efc2f68a700764bd
Alkaseem <alkaseemabubakar27@gmail.com>
2020-12-03 11:37:29 +0100

    Applicantt profile

--- src/Routes.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // Roots
-  home: "/",
-  login: "/login",
-  dashboard: "/dashboard",
-  // Nested Dashboard pages
-  statistics: "/dashboard/completereg",
-  settings: "/dashboard/",
-};
